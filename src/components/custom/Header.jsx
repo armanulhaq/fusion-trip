b35854544305d0847a3bfe72061de0e1f487bded
Arman Ul Haq <72503393+armanulhaq@users.noreply.github.com>
2024-12-02 16:25:57 +0530

    change primary color pallete

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -2,10 +2,10 @@ import { Button } from "../ui/button";
 
 const Header = () => {
     return (
-        <div className="p-3 shadow-sm flex justify-between items-center px-10">
+        <div className="p-3 shadow-sm flex justify-between items-center px-5">
             <div className="flex items-center gap-3">
                 <img className="w-8 h-8" src="/logo.png" alt="" />
-                <div className="font-bold">Fusion Trip</div>
+                <div className="font-bold">Fusion</div>
             </div>
             <div>
                 <Button>Sign In</Button>
--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     return (
         <div className="flex flex-col items-center mx-56 gap-9">
             <h1 className="font-extrabold text-[60px] text-center mt-16">
-                <span className="text-[#6366f1]">
+                <span className="text-[#fb8500]">
                     Discover Your Next Adventure with AI:
                 </span>{" "}
                 Personalised Itineraries at Your Fingertips{" "}
