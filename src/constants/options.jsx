26d261c672e9e0cb61f0c28432a34fc8e299d24f
Arman Ul Haq <72503393+armanulhaq@users.noreply.github.com>
2024-12-03 12:13:24 +0530

    add layout in create-trip route

---
+++ src/constants/options.jsx
@@ -0,0 +1,51 @@
+export const SelectTravelList = [
+    {
+        id: 1,
+        title: "Just Me",
+        desc: "Solo traveler in exploration",
+        icon: "✈️",
+        people: "1",
+    },
+    {
+        id: 2,
+        title: "A couple",
+        desc: "Two travelers in tandem",
+        icon: "🥂",
+        people: "2 People",
+    },
+    {
+        id: 3,
+        title: "Family",
+        desc: "A group of adventurers",
+        icon: "🏠",
+        people: "3 to 5 People",
+    },
+    {
+        id: 4,
+        title: "Friends",
+        desc: "A bunch of thrill seekers",
+        icon: "🚤",
+        people: "5 - 10 People",
+    },
+];
+
+export const SelectBudgetOptions = [
+    {
+        id: 1,
+        title: "Cheap",
+        desc: "Stay conscious of costs",
+        icon: "💵",
+    },
+    {
+        id: 2,
+        title: "Moderate",
+        desc: "Keep cost on the average side",
+        icon: "💸",
+    },
+    {
+        id: 3,
+        title: "Luxury",
+        desc: "Don't worry about the cost",
+        icon: "💰",
+    },
+];
--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,13 +1,15 @@
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 import { useState } from "react";
 import { Input } from "@/components/ui/input";
+import { SelectBudgetOptions, SelectTravelList } from "@/constants/options";
+import { Button } from "@/components/ui/button";
 const CreateTrip = () => {
     const [place, setPlace] = useState();
     return (
         // default padding-5 and margin-top-10
         <div className="sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10">
             <h2 className="font-bold text-3xl">
-                Tell us your travel preferences
+                Tell us your travel preferences 🏕️
             </h2>
             <p className="mt-3 text-gray-500 text-xl">
                 Just provide some basic information, and our trip planner will
@@ -15,7 +17,7 @@ const CreateTrip = () => {
             </p>
             <div className="mt-20 flex flex-col gap-10">
                 <div>
-                    <h2 className="text-xl my-3 font-normal">
+                    <h2 className="text-xl my-3 font-bold">
                         What is your destination of choice?
                     </h2>
                     <GooglePlacesAutocomplete
@@ -30,11 +32,50 @@ const CreateTrip = () => {
                     />
                 </div>
                 <div>
-                    <h2 className="text-xl my-3 font-normal">
+                    <h2 className="text-xl my-3 font-bold">
                         How many days are you planning your trip?
                     </h2>
                     <Input placeholder={"Example: 2"} type="number" />
                 </div>
+            </div>
+            <div>
+                <h2 className="text-xl my-3 font-bold">What is your Budget?</h2>
+                <div className="grid grid-cols-3 gap-5 mt-5">
+                    {SelectBudgetOptions.map((item, index) => (
+                        <div
+                            key={index}
+                            className="p-4 border cursor-pointer rounded-lg hover:shadow"
+                        >
+                            <h2 className="4xl ">{item.icon}</h2>
+                            <h2 className="font-bold text-lg">{item.title}</h2>
+                            <h2 className="text-sm text-gray-500">
+                                {item.desc}
+                            </h2>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div>
+                <h2 className="text-xl my-3 font-bold">
+                    Who do you plan on traveling with on your next adventure?{" "}
+                </h2>
+                <div className="grid grid-cols-3 gap-5 mt-5">
+                    {SelectTravelList.map((item, index) => (
+                        <div
+                            key={index}
+                            className="p-4 border cursor-pointer rounded-lg hover:shadow"
+                        >
+                            <h2 className="4xl ">{item.icon}</h2>
+                            <h2 className="font-bold text-lg">{item.title}</h2>
+                            <h2 className="text-sm text-gray-500">
+                                {item.desc}
+                            </h2>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className="my-10 justify-end flex">
+                <Button>Generate Trip</Button>
             </div>
         </div>
     );
